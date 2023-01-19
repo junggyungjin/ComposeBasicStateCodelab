2e48976aa79d06af2c6a294840aa4a19522e35f3
junggyungjin <junggyungjin@naver.com>
2023-01-19 21:11:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/composebasicstatecodelab/WaterCounter.kt
@@ -0,0 +1,40 @@
+package com.example.composebasicstatecodelab
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun StatelessCounter(count: Int, onIcrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+        Button(onClick = onIcrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text(text = "Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by remember {
+        mutableStateOf(0)
+    }
+
+    StatelessCounter(count, {count++})
+
+}
